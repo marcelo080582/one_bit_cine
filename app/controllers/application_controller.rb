e66d7b75986fedff75ad30cdb38960cdc5dedc59
Marcelo Martins <marcelo.martins@cilia.com.br>
2018-07-30 22:39:30 -0300

    finish settings and added icon of change language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def set_locale
+    session[:locale] = params[:locale] if params[:locale].present?
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
 end
